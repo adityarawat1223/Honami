595d3486fc58d05374ab21b969af13e779d907bc
Aditya Rawat <burstingfire2005@gmail.com>
2023-06-14 07:38:21 +0530

    Update honami.js

--- honami.js
+++ honami.js
@@ -44,7 +44,7 @@ for (const folder of commandFolders) {
 // const eventsPath = path.join(__dirname, 'events');
 // const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-// for (const file of eventFiles) {
+// for a(const file of eventFiles) {
 // 	const filePath = path.join(eventsPath, file);
 // 	const event = require(filePath);
 // 	if (event.once) {

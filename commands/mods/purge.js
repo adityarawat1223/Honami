a93d3d5cb99c14590d49fe2e0f4e9d73b40ac9b3
adityarawat1223 <burstingfire2005@gmail.com>
2023-06-18 18:25:37 +0530

    fixing error in purge

--- commands/mods/purge.js
+++ commands/mods/purge.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder  , PermissionFlagsBits} = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     cooldown: 10,
@@ -6,13 +6,14 @@ module.exports = {
         .setName('purge')
         .setDescription('Delete message in bulk Max 100 per command').addNumberOption(
             option => option.setName("amount").setDescription("Enter message amount you want to delete").setRequired(true)
-        ).setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+        ).setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    ,
     async execute(interaction) {
         const { channel, options } = interaction
         const num = options.getNumber("amount")
         if (100 >= num) {
-            channel.bulkDelete(num)
-            interaction.reply({ content: `Deleted ${num} Messages`, ephemeral: true })
+            channel.bulkDelete(num, true)
+            interaction.reply({ content: `Deleted ${num} Messages Remembed i cant delete message older than 14 days due to discord api limitations `, ephemeral: true })
 
         }
         else {
